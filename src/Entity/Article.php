003f430832a380defb9d7fe13744d5ad99584c81
Ousmane THIAM <ousmanemoussathiam@yahoo.fr>
2021-10-06 15:36:34 +0200

    implement entities

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,8 +2,9 @@
 
 namespace App\Entity;
 
-use App\Repository\ArticleRepository;
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ArticleRepository;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -27,6 +28,12 @@ class Article
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="articles")
      */
     private $author;
+
+    public function __construct()
+    {
+       
+        $this->createdAt=new DateTime();
+    }
 
 
     public function getName(): ?string
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -42,6 +43,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function __construct()
     {
         $this->articles = new ArrayCollection();
+        $this->createdAt=new DateTime();
     }
 
     
